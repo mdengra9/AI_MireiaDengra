c082678e55b0218d3993ae4362c800c4463ef5cf
mdengra9 <mireia.den.esp@gmail.com>
2024-01-26 10:50:12 +0100

    gf

--- Assets/Scripts/IAenemy.cs
+++ Assets/Scripts/IAenemy.cs
@@ -19,6 +19,9 @@ public class IAenemy : MonoBehaviour
     [SerializeField] Transform patrolAreaCenter;
     [SerializeField] Vector2 patrolAreaSize;
 
+    [SerializeField] float visionRange = 15;
+    [SerializeField] float visionAngle = 90;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -48,6 +51,11 @@ public class IAenemy : MonoBehaviour
 
     void Patrol()
     {
+        if(OnRange() == true)
+        {
+            currentState = State.Chasing;
+        }
+
         if(enemyAgent.remainingDistance < 0.5f)
         {
             SetRandomPoint();
@@ -56,21 +64,55 @@ public class IAenemy : MonoBehaviour
 
     void Chase()
     {
+        enemyAgent.destination = playerTransform.position;
 
+        if(OnRange() == false)
+        {
+            currentState = State.Patrolling;
+        }
     }
 
     void SetRandomPoint()
     {
         float randomX = Random.Range(-patrolAreaSize.x / 2, patrolAreaSize.x / 2);
         float randomZ = Random.Range(-patrolAreaSize.y / 2, patrolAreaSize.y / 2);
-        Vector3 randomPoint = new Vector3(randomX, 0f, randomZ);
+        Vector3 randomPoint = new Vector3(randomX, 0f, randomZ) + patrolAreaCenter.position;
 
         enemyAgent.destination = randomPoint;
+    }
+
+    bool OnRange()
+    {
+       /*if (Vector3.Distance(transform.position, playerTransform.position) <= visionRange)
+       {
+        return true;
+       } 
+       return false;*/
+
+       Vector3 directionToPlayer = playerTransform.position - transform.position;
+       float distanceToPlayer = directionToPlayer.magnitude;
+       float angleToPlayer = Vector3.Angle(transform.forward, directionToPlayer);
+
+       if(distanceToPlayer <= visionRange && angleToPlayer < visionAngle * 0.5f)
+       {
+        return true;
+       }
+       return false;
     }
 
     void OnDrawGizmos()
     {
         Gizmos.color = Color.blue;
         Gizmos.DrawWireCube(patrolAreaCenter.position, new Vector3(patrolAreaSize.x, 0, patrolAreaSize.y));
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, visionRange);
+
+        Gizmos.color = Color.green;
+        Vector3 fovLine1 = Quaternion.AngleAxis(visionAngle * 0.5f, transform.up) * transform.forward * visionRange;
+        Vector3 fovLine2 = Quaternion.AngleAxis(-visionAngle * 0.5f, transform.up) * transform.forward * visionRange;
+
+        Gizmos.DrawLine(transform.position, transform.position + fovLine1);
+        Gizmos.DrawLine(transform.position, transform.position + fovLine2);
     }
 }
